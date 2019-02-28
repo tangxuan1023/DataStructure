816101ae3c897aca3e0e0f8ac0191d7cae91c181
tangxuan1023 <yuzhixuan_1009@sina.com>
2019-02-28 16:52:34 +0800

    update queue dynamic impl

--- Project1/queue_dynamic.cpp
+++ Project1/queue_dynamic.cpp
@@ -3,57 +3,105 @@
 #include "queue.h"
 #ifdef _DYNAMIC_QUEUE_
 
-static size_t queue_size;
-static size_t array_size;
+// C program for array implementation of queue 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <limits.h> 
 
-static QUEUE_TYPE *queue;
-static size_t front = 1;
-static size_t rear = 0;
-
-void create_queue(size_t size)
+// A structure to represent a queue 
+struct Queue
 {
-	assert(queue_size == 0);
-	queue_size = size;
-	array_size = queue_size + 1;
-	queue = (QUEUE_TYPE*)malloc(array_size * sizeof(QUEUE_TYPE));
-	assert(queue != NULL);
+    int front, rear, size;
+    unsigned capacity;
+    int* array;
+};
+
+// function to create a queue of given capacity. 
+// It initializes size of queue as 0 
+struct Queue* createQueue(unsigned capacity)
+{
+    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue));
+    queue->capacity = capacity;
+    //queue->front = 0;
+    queue->front = 1;
+    queue->size = 0;
+    //queue->rear = capacity - 1; // This is important, see the enqueue 
+    queue->rear = 0;
+    queue->array = (int*)malloc(queue->capacity * sizeof(int));
+    return queue;
 }
 
-void destroy_queue(void)
+// Queue is full when size becomes equal to the capacity 
+int isFull(struct Queue* queue)
 {
-	assert(queue_size > 0);
-	queue_size = 0;
-	free(queue);
-	queue = NULL;
+    return (queue->rear + 2) % queue->capacity == queue->front;
+    //return (queue->size == queue->capacity);
 }
 
-void insert(QUEUE_TYPE value)
+// Queue is empty when size is 0 
+int isEmpty(struct Queue* queue)
 {
-	assert(!is_queue_full());
-	rear = (rear + 1) % array_size;
-	queue[rear] = value;
+    return (queue->rear + 1) % queue->capacity == queue->front;
+    //return (queue->size == 0);
 }
 
-void delete_(void)
+// Function to add an item to the queue. 
+// It changes rear and size 
+void enqueue(struct Queue* queue, int item)
 {
-	assert(!is_queue_empty());
-	front = (front + 1) % array_size;
+    if (isFull(queue))
+        return;
+    queue->rear = (queue->rear + 1) % queue->capacity;
+    queue->array[queue->rear] = item;
+    queue->size = queue->size + 1;
+    printf("%d enqueued to queue\n", item);
 }
 
-QUEUE_TYPE first(void)
+// Function to remove an item from queue. 
+// It changes front and size 
+int dequeue(struct Queue* queue)
 {
-	assert(!is_queue_empty());
-	return queue[front];
+    if (isEmpty(queue))
+        return INT_MIN;
+    int item = queue->array[queue->front];
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size = queue->size - 1;
+    return item;
 }
 
-int is_queue_empty(void)
+// Function to get front of queue 
+int front(struct Queue* queue)
 {
-	return (rear + 1) % array_size == front;
+    if (isEmpty(queue))
+        return INT_MIN;
+    return queue->array[queue->front];
 }
 
-int is_queue_full(void)
+// Function to get rear of queue 
+int rear(struct Queue* queue)
 {
-	return (rear + 2) % array_size == front;
+    if (isEmpty(queue))
+        return INT_MIN;
+    return queue->array[queue->rear];
+}
+
+// Driver program to test above functions./ 
+int test_dy_queue()
+{
+    struct Queue* queue = createQueue(1000);
+
+    enqueue(queue, 10);
+    enqueue(queue, 20);
+    enqueue(queue, 30);
+    enqueue(queue, 40);
+
+    printf("%d dequeued from queue\n\n", dequeue(queue));
+
+    printf("Front item is %d\n", front(queue));
+    printf("Rear item is %d\n", rear(queue));
+
+    getchar();
+    return 0;
 }
 
 #endif
